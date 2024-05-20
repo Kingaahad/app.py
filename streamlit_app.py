5202e277bb41c656ae4fd161b47428ab0f2ada03
Kingaahad <daniyalasghar150@gmail.com>
2024-05-21 02:12:31 +0500

    Update streamlit_app.py

--- streamlit_app.py
+++ streamlit_app.py
@@ -2,6 +2,28 @@ import streamlit as st
 import pandas as pd
 import numpy as np
 
+DATA_URL = ("/This PC/Desktop/home/Motor_Vehicle_Collisions_-_Crashes.csv")
+
+st.title("Motor Vehicle Collisions in New York City")
+st.markdown("This application is a Streamlit dashboard that can be used to analyze motor vehicle collisions in NYC🗽💥🚗")
+
+@st.cache(persist=True)
+def load_data(nrows):
+    data = pd.read_csv(DATA_URL, nrows=nrows, parse_dates=[['CRASH_DATE', 'CRASH_TIME']])
+    data.dropna(subset=['LATITUDE', 'LONGITUDE'], inplace=True)
+    lowercase = lambda x: str(x).lower()
+    data.rename(lowercase, axis='columns', inplace=True)
+    data.rename(columns={'crash_date_crash_time': 'date/time'}, inplace=True)
+    return data
+
+data = load_data(100000)
+
+st.write(data.head())
+
+import streamlit as st
+import pandas as pd
+import numpy as np
+
 DATA_URL= ("/This PC/Desktop/home/Motor_Vehicle_Collisions_-_Crashes.csv"
 )
 st.title("Motor Vehicle Collisions in New York City")
