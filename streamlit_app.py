cb2df2f562bdcb575a23032e1c5419983d5087bd
Kingaahad <daniyalasghar150@gmail.com>
2024-05-21 01:30:52 +0500

    Update streamlit_app.py

--- streamlit_app.py
+++ streamlit_app.py
@@ -4,5 +4,7 @@ import numpy as np
 
 DATA_URL= ("/This PC/Desktop/home/Motor_Vehicle_Collisions_-_Crashes.csv"
 )
-
+st.title("Motor Vehicle Collisions in New York City")
+st.markdown("This application is a Streamlit dashboard that can be used"
+"to analyze motor vehicle collisions in NYC🗽💥🚗")
 st.write("hello, world")
