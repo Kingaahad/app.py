30b0e12f405a4addedf9f121e9c5af3c5a96d1c4
Kingaahad <daniyalasghar150@gmail.com>
2024-05-21 01:23:19 +0500

    Update streamlit_app.py

--- streamlit_app.py
+++ streamlit_app.py
@@ -2,7 +2,7 @@ import streamlit as st
 import pandas as pd
 import numpy as np
 
-DATA_URL= ("/This PC/Desktop/home/Motor_Vehicle_Collisions_-_Crashes.csv
+DATA_URL= ("/This PC/Desktop/home/Motor_Vehicle_Collisions_-_Crashes.csv"
 )
 
 st.write("hello, world")
