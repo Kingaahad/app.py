d6f404db9351711e41a9b0fefbd071d0e60e3a81
Kingaahad <daniyalasghar150@gmail.com>
2024-05-21 12:16:28 +0500

    Update streamlit_app.py

--- streamlit_app.py
+++ streamlit_app.py
@@ -1,28 +1,23 @@
 import streamlit as st
 import pandas as pd
 import numpy as np
-
+# Set the Streamlit title and description
+st.title("Motor Vehicle Collisions in New York City")
+st.markdown("This application is a Streamlit dashboard that can be used "
+             "to analyze motor vehicle collisions in NYC🗽💥🚗")
 # Set the data URL
 DATA_URL = ("/Users/Kingaahad/This PC/Downloads/Motor_Vehicle_Collisions_-_Crashes.csv"
 )
 # Define the function to load the data
 def load_data(nrows):
-    """Load the motor vehicle collision data with optional number of rows."""
     data = pd.read_csv(DATA_URL, nrows=nrows, parse_date=[['CRASH_DATE', 'CRASH_TIME']])
-
-    # Drop rows with missing latitude or longitude values
     data.dropna(subset=['LATITUDE', 'LONGITUDE'], inplace=True)
-
-    # Convert column names to lowercase
     lowercase = lambda x: str(x).lower()
     data.rename(lowercase, axis='columns', inplace=True)
-
-    # Rename the date/time column
     data.rename(columns={'crash_date_crash_time': 'date/time'}, inplace=True)
-
     return data
 
-# Load the data with 100,000 rows
+
 data = load_data(100000)
 
 # Set the Streamlit title and description
