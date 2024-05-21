d7a7ab34bbfd86eaee500492eda7f8d2905cdbbd
Kingaahad <daniyalasghar150@gmail.com>
2024-05-21 11:39:51 +0500

    Update streamlit_app.py

--- streamlit_app.py
+++ streamlit_app.py
@@ -2,7 +2,7 @@ import streamlit as st
 import pandas as pd
 import numpy as np
 
-DATA_URL= ("Users/Kingaahad/This PC/Download/Motor_Vehicle_Collisions_-_Crashes.csv"
+DATA_URL= ("/Users/Kingaahad/This PC/Download/Motor_Vehicle_Collisions_-_Crashes.csv"
 )
 st.title("Motor Vehicle Collisions in New York City")
 st.markdown("This application is a Streamlit dashboard that can be used "
