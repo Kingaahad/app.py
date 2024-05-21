2e95525f73a0da4cc90469589f835baf804d98e3
Kingaahad <daniyalasghar150@gmail.com>
2024-05-21 14:55:30 +0500

    Update streamlit_app.py

--- streamlit_app.py
+++ streamlit_app.py
@@ -1,45 +1,9 @@
-import streamlit as st
-import pandas as pd
-import numpy as np
-# Set the Streamlit title and description
-st.title("Motor Vehicle Collisions in New York City")
-st.markdown("This application is a Streamlit dashboard that can be used "
-             "to analyze motor vehicle collisions in NYC🗽💥🚗")
-# Set the data URL
-DATA_URL = ("/Users/Kingaahad/This PC/Downloads/Motor_Vehicle_Collisions_-_Crashes.csv"
-)
-# Define the function to load the data
-def load_data(nrows):
-    data = pd.read_csv(DATA_URL, nrows=nrows, parse_date=[['CRASH_DATE', 'CRASH_TIME']])
-    data.dropna(subset=['LATITUDE', 'LONGITUDE'], inplace=True)
-    lowercase = lambda x: str(x).lower()
-    data.rename(lowercase, axis='columns', inplace=True)
-    data.rename(columns={'crash_date_crash_time': 'date/time'}, inplace=True)
-    return data
-
-
-data = load_data(100000)
-
-# Set the Streamlit title and description
-st.title("Motor Vehicle Collisions in New York City")
-st.markdown("This application is a Streamlit dashboard that can be used "
-             "to analyze motor vehicle collisions in NYC🗽💥🚗")
-
-# Display a header and a slider to select the number of injured persons
-st.header("Where are the most people injured in NYC")
-injured_people = st.slider("Numbers of persons injured in vehicle collisions", 0, 19)
-
-# Display the map of collision locations with the selected number of injured persons
-st.map(data.query("injured_persons >= @injured_people")[["latitude", "longitude"]].dropna(how="any"))
-
-
-
 
 import streamlit as st
 import pandas as pd
 import numpy as np
 
-DATA_URL= ("/Users/Kingaahad/This PC/Downloads/Motor_Vehicle_Collisions_-_Crashes.csv"
+DATA_URL= ("C:\Users\Kingaahad\Desktop\home/Motor_Vehicle_Collisions_-_Crashes.csv"
 )
 st.title("Motor Vehicle Collisions in New York City")
 st.markdown("This application is a Streamlit dashboard that can be used "
